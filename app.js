4405592ad59096101a23c219b9e35e913a8f677b
Codage-of-Param <paramchotaliya56@gmail.com>
2026-07-11 13:58:15 +0530

    API generation

---
+++ app.js
@@ -0,0 +1,20 @@
+// install cors and express
+// CORS : Cross origin resource service
+
+let express = require('express');
+let cors = require('cors');
+
+let app = express()
+let port = 9000
+
+app.use(cors());
+
+app.get('/ping', (req,res) => {
+    res.send({
+        message: "Hello World!"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+});
